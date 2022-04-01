77811595c5966dc979473157c583bbade61dba5a
Yoviar Pauzi <yoviarpauzi99@gmail.com>
2022-04-01 12:14:12 +0700

    replace some code

--- sesi-1/convert-suhu-fahrenheit-to-celcius.c
+++ sesi-1/convert-suhu-fahrenheit-to-celcius.c
@@ -4,7 +4,7 @@ int main(){
     float fahrenheit, celcius;
     printf("Inputkan suhu fahrenheit : ");
     scanf("%f", &fahrenheit);
-    celcius = ((fahrenheit - 32.0) * 5.0 / 9.0);
+    celcius = ((fahrenheit - 32.0) * (5.0 / 9.0));
     printf("Hasil convert suhu = %.2f°C\n", celcius);
     return 0;
 }
