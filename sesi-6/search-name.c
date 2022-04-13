7e479331ce1486e67653a04324fe2c933d7b527c
Yoviar Pauzi <yoviarpauzi99@gmail.com>
2022-04-13 18:53:00 +0700

    initial commit

--- sesi-6/search-name.c
+++ sesi-6/search-name.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #define N 8
 int main(){
-    int juml, b, c;
+    int b = 0;
     char data[N][15] = {"siti markonah", "siti marsiah", "siti ryzen", "ahdan", "rio", 
     "agus", "bagus", "muhammad agus"};
     char key[15];
@@ -16,32 +16,12 @@ int main(){
     }
     printf("\n\nInputkan kata kunci : ");
     scanf("%s", &key);
-    b = strlen(key);
-    printf("\nMenampilkan data yang mengandung kata kunci : \n");
+    printf("\nMenampilkan hasil pencarian yang mengandung kata kunci : \n");
     for(int i = 0; i < N; i++){
-        loop: 
-        if (strlen(data[i])>= b){
-            for (int j = 0; j <= strlen(data[i]) - b; j++){
-                for (int k = 0; k < b; k++){
-                    if (key[k] == data[i][k + j]){
-                        if(k == b-1){
-                            c += 1;
-                            printf("%s\n", data[i]);
-                            j = 0;
-                            i++;
-                            if ( i < N)
-                                goto loop;
-                            else   
-                                return 0;
-                        }
-                    }else
-                        break;
-                }
-            }
+        if(strstr(data[i], key)){
+            b += 1;
+            printf("%s\n", data[i]);
         }
-    }
-    if (c == 0){
-        printf("Data tidak ditemukan\n");
     }
     return 0;
 }
